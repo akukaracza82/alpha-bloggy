ad2c3cb959032b4c5ed605617762cb6c66a74d95
Artur <amrozinski@gmail.com>
2020-10-13 13:14:16 +0100

    added show/edit/delete buttons

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,16 +12,33 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
-      flash[:notice] = "article was succefully saved"
+      flash[:notice] = "Article was succefully saved"
       redirect_to @article
     else
       render 'new'
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated succefully."
+      redirect_to article_path
+    else
+      render 'edit'
+    end
+  end
 
-
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
